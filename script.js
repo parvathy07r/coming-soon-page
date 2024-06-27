f5f0201150b63e473ad93bbd6a83a44762fab5a7
Parvathy R <parvathy07r@gmail.com>
2024-06-28 01:00:43 +0530

    Modified script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const inputText = document.querySelector("input");
 const notifyMe = document.querySelector("button");
+const errorMessage = document.querySelector(".error-message");
 
 const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-notifyMe.addEventListener("click", function(event) {
+notifyMe.addEventListener("click", function (event) {
     console.log(inputText.value);
-    if(inputText.value.length === 0 || !validRegex.test(inputText.value)) {
+    if (inputText.value.length === 0 || !validRegex.test(inputText.value)) {
         console.log("Error!!!!");
+        createErrorMessage();
+    } else {
+        errorMessage.innerHTML = "";
+        inputText.value = ""
+        console.log("Submitted!!!");
+    }
+});
+
+function createErrorMessage() {
+    if (errorMessage.innerHTML === '') {
         inputText.style.border = "1px solid red";
         const node = document.createElement("p");
         const textNode = document.createTextNode("Please provide a valid email address");
@@ -16,7 +27,20 @@ notifyMe.addEventListener("click", function(event) {
         node.style.textAlign = "left";
         node.append(textNode);
         document.querySelector(".error-message").append(node);
+        console.log("gsdfghsdfh")
     }else {
-        console.log("Submitted!!!");
+        errorMessage.innerHTML = "";
+        inputText.style.border = "1px solid red";
+        const node = document.createElement("p");
+        const textNode = document.createTextNode("Please provide a valid email address");
+        node.style.color = "red";
+        node.style.fontStyle = "italic";
+        node.style.fontSize = "10px";
+        node.style.textAlign = "left";
+        node.append(textNode);
+        document.querySelector(".error-message").append(node);
+        console.log("hdftwt")
     }
-});
+
+}
+
